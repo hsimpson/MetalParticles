6c0adee7f750eb3244b2239877ce08bf6c1854ac
Daniel Toplak <homer.simpson123@gmx.de>
2022-01-24 20:59:16 +0100

    Initial Commit

---
+++ MetalParticles/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MetalParticles
+//
+//  Created by Daniel Toplak on 24.01.22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
